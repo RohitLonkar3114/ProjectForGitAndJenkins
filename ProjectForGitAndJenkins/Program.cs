fe1a60ecb7377d0771a191264446b375ed0e6d30
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2026-02-07 10:24:25 +0530

    Add project files.

---
+++ ProjectForGitAndJenkins/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGitAndJenkins
+{
+    public class Program
+    {
+        public void Addition()
+        {
+            int a = 20;
+            int b = 10;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
